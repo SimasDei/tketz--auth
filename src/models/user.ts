bc2d42f9fb124a512eeb0d07829f6c89129be588
Simas <simas@simas-dei.com>
2021-04-12 18:33:24 +0300

    add password hashing on pre save mongoose hook add bad request error class

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,17 @@
 import mongoose from 'mongoose';
 
+import { Password } from '../services';
+
 interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDocument> {
+  build: (attrs: UserAttrs) => UserDocument;
+}
+
+interface UserDocument extends mongoose.Document {
   email: string;
   password: string;
 }
@@ -16,10 +27,17 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
 
-export const buildUser = (attrs: UserAttrs) => {
-  return new User(attrs);
-};
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+
+const User = mongoose.model<UserDocument, UserModel>('User', userSchema);
 
 export { User };
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
-import { DatabaseConnectionError, RequestValidatonError } from '../errors';
+import { RequestValidatonError } from '../errors';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models';
 
 const router = express.Router();
 
@@ -11,17 +13,23 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidatonError(errors.array());
     }
     const { email, password } = req.body;
 
-    console.log('Creating a user with email', email);
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError('Email in use 🙈');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
